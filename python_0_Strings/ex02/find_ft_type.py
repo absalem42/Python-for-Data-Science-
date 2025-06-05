c87c0708913493786b413f49469d5a3122e45b08
absalem42 <absalem42@gmail.com>
2025-06-05 14:42:10 +0400

    Remove unused files and clean up all_thing_is_obj function for improved type checking

--- python_0_Strings/ex02/find_ft_type.py
+++ python_0_Strings/ex02/find_ft_type.py
@@ -1,30 +1,16 @@
-# def all_thing_is_obj(object: any) -> int:
-#     print(type(object))
-#     return 42
-
-
 def all_thing_is_obj(object: any) -> int:
-    if object is str:
+    if isinstance(object, list):  # Check if object is a list
         print(f"List : {type(object)}")
-    elif object is tuple:
+    elif isinstance(object, tuple):  # Check if object is a tuple
         print(f"Tuple : {type(object)}")
-    elif object is set:
+    elif isinstance(object, set):  # Check if object is a set
         print(f"Set : {type(object)}")
-    elif object is dict:
+    elif isinstance(object, dict):  # Check if object is a dictionary
         print(f"Dict : {type(object)}")
-    elif object == "Brian":
+    elif object == "Brian":  # Check if object is the string "Brian"
         print(f"Brian is in the kitchen : {type(object)}")
-    elif object == "Toto":
+    elif object == "Toto":  # Check if object is the string "Toto"
         print(f"Toto is in the kitchen : {type(object)}")
     else:
         print("Type not found$")
     return 42
-
-
-# all_thing_is_obj(ft_list)
-# all_thing_is_obj(ft_tuple)
-# all_thing_is_obj(ft_set)
-# all_thing_is_obj(ft_dict)
-# all_thing_is_obj("Brian")
-# all_thing_is_obj("Toto")
-# print(all_thing_is_obj(10))
