8a4dc903c4b7723ffc79235ddaa237fa1f69d661
absalem42 <absalem42@gmail.com>
2025-06-11 14:57:27 +0400

    Fix small issue with string ex02

--- python_0_Strings/ex02/find_ft_type.py
+++ python_0_Strings/ex02/find_ft_type.py
@@ -7,10 +7,8 @@ def all_thing_is_obj(object: any) -> int:
         print(f"Set : {type(object)}")
     elif isinstance(object, dict):  # Check if object is a dictionary
         print(f"Dict : {type(object)}")
-    elif object == "Brian":  # Check if object is the string "Brian"
-        print(f"Brian is in the kitchen : {type(object)}")
-    elif object == "Toto":  # Check if object is the string "Toto"
-        print(f"Toto is in the kitchen : {type(object)}")
+    elif isinstance(object, str):  # Check if object is the string "Brian"
+        print(f"{object} is in the kitchen : {type(object)}")
     else:
         print("Type not found$")
     return 42
