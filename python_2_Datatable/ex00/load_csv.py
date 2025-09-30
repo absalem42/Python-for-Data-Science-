b89b55a98d13c01ea13f12dda50f40f38f3235bb
absalem42 <absalem42@gmail.com>
2025-09-30 18:07:56 +0400

    Add load function to read CSV files and print dataset dimensions

---
+++ python_2_Datatable/ex00/load_csv.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+
+def load(path: str):
+    try:
+        data = pd.read_csv(path)
+
+        print("Loading dataset of dimensions ...", data.shape())
+
+        return data
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+
+
+
+
+if __name__ == "__main__":
+    load(" life_expectancy_years.csv")
+    
+
+
+
+
+
+
+
+
+
+
+# i need to write the dimensions of the data set after oepning the file 
