df6a8277acce69f9489e1e87f8a417d752c01674
Abdullah Ali Salem <absalem@lab1r6s10.42abudhabi.ae>
2026-01-18 17:26:05 +0400

    Implement Baratheon and Lannister classes; refactor Stark class die method

---
+++ python_3_OOP/ex01/S1E7.py
@@ -0,0 +1,22 @@
+from S1E9 import Character
+
+class Baratheon(Character):
+    def __init__(self, first_name, is_alive=True):
+        self.first_name = first_name
+        self.is_alive = is_alive
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def __str__(self):
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+    def __repr__(self):
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+
+class Lannister(Character):
+
+        # decorator
+    def create_lannister():
+        pass
+
